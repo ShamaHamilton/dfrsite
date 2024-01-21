98d66be1d7b9dbd9c49c768cccac90f4e6737e7d
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-01-21 19:52:24 +0300

    feat: Class Serializer

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	./venv/Scripts/python.exe ./drfsite/manage.py runserver 8000
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -1,10 +1,24 @@
+from django.forms import model_to_dict
 from rest_framework.generics import ListAPIView
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from women.serializers import WomenSerializer
 from women.models import Women
 
 
-class WomenAPIView(ListAPIView):
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+class WomenAPIView(APIView):
+    def get(self, request):
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
+
+    def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Women.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': WomenSerializer(post_new).data})
