169f088632a49e89ef94b4636e8bdc46d9c8e5b3
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2024-02-10 14:20:01 +0300

    feat: SimpleRouter and DefaultRouter.

--- drfsite/config/urls.py
+++ drfsite/config/urls.py
@@ -21,8 +21,26 @@ from rest_framework import routers
 from women.views import WomenViewSet
 
 
-router = routers.SimpleRouter()
-router.register(r'women', WomenViewSet)
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get': 'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix': 'List'}),
+        routers.Route(url=r'^{prefix}/{lookup}$',
+                      mapping={'get': 'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix': 'Detail'}),
+    ]
+
+
+# router = routers.SimpleRouter()
+router = routers.DefaultRouter()
+# router = MyCustomRouter()
+router.register(r'women', WomenViewSet, basename='women')
+# print(router.urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -8,15 +8,32 @@ from rest_framework.generics import (
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from women.serializers import WomenSerializer
-from women.models import Women
+from women.models import Category, Women
 
 
 class WomenViewSet(ModelViewSet):
-    queryset = Women.objects.all()
+    # queryset = Women.objects.all()  # нужно указать basename, если нет queryset
     serializer_class = WomenSerializer
 
+    def get_queryset(self):
+        pk = self.kwargs.get('pk')
+
+        if not pk:
+            return Women.objects.all()[:3]
+        return Women.objects.filter(pk=pk)
+
+    # @action(methods=['get'], detail=False)  # добавление нового url
+    # def category(self, request):            # .../api/v1/women/category
+    #     cats = Category.objects.all()
+    #     return Response({'cats': [c.name for c in cats]})
+
+    @action(methods=['get'], detail=True)  # получение конкретной категории
+    def category(self, request, pk=None):  # .../api/v1/women/1/category/
+        cats = Category.objects.get(pk=pk)
+        return Response({'cats': cats.name})
 
 # class WomenAPIList(ListCreateAPIView):
 #     queryset = Women.objects.all()
